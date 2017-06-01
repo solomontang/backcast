a6ab2a9a322d51d2f8c4e5e545f321d87dc74748
solomontang <solomoncztang@gmail.com>
2017-06-01 10:34:16 -0700

    Refactor App rendering

--- src/views/app.js
+++ src/views/app.js
@@ -3,15 +3,19 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    this.videos = new Videos(window.exampleVideoData);
+    
   },
 
 
   render: function() {
     this.$el.html(this.template());
+    new SearchView().render();
+    new VideoPlayerView().render();
+    new VideoListView().render();
     return this;
   },
 
   template: templateURL('src/templates/app.html')
-
+  
 });
